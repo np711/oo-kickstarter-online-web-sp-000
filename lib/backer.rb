a49d916e05c7e767179cf3ab4ad083978e002b39
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2020-02-03 17:28:29 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,14 @@
 class Backer
-  attr_accessor :name, :backed_projects
-  
+  attr_reader :name, :backed_projects
+
   def initialize(name)
-    @name = name 
+    @name = name
     @backed_projects = []
-  end 
-  
-  def backed_projects(project)
-    @backed_projects << project 
-    project.add_backer(self) unless project.backers.include?(self)
   end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
+  end
+
 end
